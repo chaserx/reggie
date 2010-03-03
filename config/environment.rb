bbff1cd0b8bca7c9e6083094bd6085850704bee6
Chase Southard <chase.southard@gmail.com>
2010-03-03 13:49:27 -0500

    removed some depedncies

--- config/environment.rb
+++ config/environment.rb
@@ -23,9 +23,7 @@ Rails::Initializer.run do |config|
   config.gem "fastercsv"
   config.gem "hoptoad_notifier"
   config.gem "will_paginate"
-  config.gem "zippy", :source => "http://gems.github.com", :lib => "zippy"
-  config.gem "httparty"
-
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -45,6 +43,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   require 'fastercsv'
-  require 'zip/zip'
-  require 'zip/zipfilesystem'
+  
 end
