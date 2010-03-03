0d4f4f5e72d0ae56f83239add802e6f8c6227455
Chase Southard <chase.southard@gmail.com>
2010-03-03 14:33:35 -0500

    created semistatic pages

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,20 @@
+class PageController < ApplicationController
+  def index
+  end
+  
+  def about
+  end
+  
+  def travel
+  end
+
+  def privacy
+  end
+
+  def schedule
+  end
+
+  def terms
+  end
+
+end
